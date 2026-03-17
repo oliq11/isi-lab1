1079e13ace17beb2c09603c3a22f10483d1bcc91
Oliwia <oliwia_spa2005@interia.pl>
2026-03-17 20:39:45 +0100

    Adjusted everything to work correctly

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Serwer działa!');
+});
+
 app.listen(port, () => {
   console.log(`Serwer działa na http://localhost:${port}`);
 });
