0da5240f12ce4459d729a7370be63da25bb23023
Oliwia <oliwia_spa2005@interia.pl>
2026-04-07 17:31:00 +0200

    Initialize database and basic administrative access

--- blog/index.js
+++ blog/index.js
@@ -2,14 +2,12 @@ import express from 'express';
 import blogRoutes from './routes.js';
 import { sequelize } from './models/Post.js';
 
-const app = express(); 
+const app = express();
 
-sequelize.sync()
-    .then(() => console.log('Baza danych SQLite została zsynchronizowana (tabele utworzone).'))
-    .catch(err => console.error('Błąd synchronizacji bazy:', err));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/blog', blogRoutes); 
+app.use('/blog', blogRoutes);
 
-app.listen(3000, () => {
-    console.log('Server runs on http://localhost:3000');
+sequelize.sync().then(() => {
+    app.listen(3000, () => console.log('http://localhost:3000/blog'));
 });
--- blog/routes.js
+++ blog/routes.js
@@ -1,8 +1,44 @@
 import express from 'express';
 const router = express.Router();
+import { Post } from './models/Post.js';
 
-router.get('/', (req, res) => {
-    res.send('Lista postów bloga');
+router.get('/add', (req, res) => {
+    res.send(`
+        <h1>Panel Admina - Dodaj Post</h1>
+        <form action="/blog/add" method="POST">
+            <input type="text" name="title" placeholder="Tytuł" required><br>
+            <textarea name="content" placeholder="Treść"></textarea><br>
+            <input type="text" name="author" placeholder="Autor" required><br>
+            <button type="submit">Zapisz w bazie</button>
+        </form>
+    `);
+});
+
+router.post('/add', async (req, res) => {
+    try {
+        const { title, content, author } = req.body;
+        await Post.create({ title, content, author });
+        res.redirect('/blog'); 
+    } catch (error) {
+        res.send("Błąd: " + error.message);
+    }
+});
+
+router.get('/', async (req, res) => {
+    const posts = await Post.findAll();
+    
+    let html = '<h1>Lista Postów</h1><a href="/blog/add">Dodaj nowy</a><hr>';
+    posts.forEach(post => {
+        html += `
+            <div>
+                <h2>${post.title}</h2>
+                <p>${post.content}</p>
+                <small>Autor: ${post.author} | Data: ${post.created_at}</small>
+            </div>
+            <hr>
+        `;
+    });
+    res.send(html);
 });
 
 export default router;
